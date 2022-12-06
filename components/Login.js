320c252e4e0664156481357f08e2b6c5499fcffb
Shawn Mathen <sgmathen@gmail.com>
2022-12-05 22:08:33 -0600

    Adding all to try and host comments

---
+++ components/Login.js
@@ -0,0 +1,129 @@
+import { useEffect, useState } from "react";
+import Seo from "./Seo";
+import styles from "../styles/login.module.css";
+import { useRouter } from "next/router";
+import jwt_decode from "jwt-decode";
+
+
+const STORAGE_KEY = "@user";
+const STORAGE_PINKEY = "@pin";
+const clientId = "163203061075-th8790psc1e625eqm27l96uiag8p3ocf.apps.googleusercontent.com";
+/**
+ * @swagger
+ * /Login:
+ *   post:
+ *     description: Queries database for an employee based on user input.
+ *     parameters:
+ *      - name: req
+ *        description: Request body with information from user's input
+ *      - name: res
+ *        description: Response body with information after authenticating user
+ *      
+ */
+export default function Login() {
+    const router = useRouter();
+    const [user, setUser] = useState("null");
+    const [role, setRole] = useState("guest");
+    const [auth, setAuth] = useState(false);
+    const [pinNum, setPinNum] = useState("");
+
+    const authUser = async (e) => {
+        e.preventDefault();
+        try {
+            const response = await fetch("https://project3-backend.onrender.com/userAuth", {
+                method: "POST",
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({ "pin": pinNum })
+            })
+                .then(response => response.json())
+                .then(response => {
+                    if (response["success"]) {
+                        setUser(response["employee_info"][0]["name"]);
+                        setRole(response["employee_info"][0]["role"]);
+                        const loginInfo = { "name": response["employee_info"][0]["name"], "role": response["employee_info"][0]["role"] };
+                        saveUser(loginInfo);
+                    } else {
+                        console.log("Auth failed!")
+                        alert("Wrong PIN number!!!");
+                    }
+                });
+
+            router.push("/auth");
+        } catch (error) {
+            console.log(error.message);
+        }
+    }
+
+    const saveUser = (userInfo) => {
+        const s = JSON.stringify(userInfo);
+        const p = JSON.stringify(pinNum);
+        localStorage.setItem(STORAGE_KEY, s);
+        localStorage.setItem(STORAGE_PINKEY, p);
+        setAuth(true);
+    }
+
+    const loadUser = () => {
+        const s = localStorage.getItem(STORAGE_KEY);
+        const p = localStorage.getItem(STORAGE_PINKEY);
+        const userInfo = JSON.parse(s);
+        if (userInfo === null) {
+            setAuth(false);
+        } else {
+            setUser(userInfo["name"]);
+            setRole(userInfo["role"]);
+            setAuth(true);
+        }
+    }
+
+    const handleCallbackResponse = (res) => {
+        let userObject = jwt_decode(res.credential);
+        setUser(userObject.name);
+        setRole("google-account");
+        setPinNum("99999");
+        setAuth(true);
+        const loginInfo = { "name": userObject.name, "role": "google-account" };
+        localStorage.setItem(STORAGE_KEY, JSON.stringify(loginInfo));
+        localStorage.setItem(STORAGE_PINKEY, JSON.stringify("99999"));
+        router.push("/auth");
+    }
+
+    useEffect(() => {
+        loadUser();
+        google.accounts.id.initialize({
+            client_id: clientId,
+            callback: handleCallbackResponse
+        });
+        google.accounts.id.renderButton(
+            document.getElementById("signInDiv"),
+            { theme: "outline", size: "large" }
+        );
+    }, [])
+
+    return (
+        <>
+            <Seo title="Login" />
+            <div className={styles.login}>
+                <div className={styles.loginWrapper}>
+                    <div className={styles.loginLeft}>
+                        <img src="logo.jpeg" className={styles.loginLogo} placeholder="logo" />
+                        <span className={styles.loginDesc}>Hola amigos! Join our team! <p className={styles.hashTag}>#justcabo</p></span>
+                    </div>
+                    <div className={styles.loginRight}>
+                        <div className={auth ? styles.welcomeText : styles.hidden}>
+                            Welcome {user}!
+                        </div>
+                        <form className={auth ? styles.hidden : styles.loginBox}>
+                            <input className={styles.loginInput} onChange={e => setPinNum(e.currentTarget.value)} value={pinNum} placeholder="PIN Number" type="password" required={true} />
+                            <button className={styles.loginButton} onClick={authUser} >Log in</button>
+                        </form>
+                        <div id="signInDiv"></div>
+                    </div>
+
+                </div>
+            </div>
+        </>
+    )
+}
---
+++ components/Map.js
@@ -0,0 +1,40 @@
+import React, { Fragment } from "react";
+import Seo from "./Seo";
+import MAP_KEY from "../secrets";
+
+const Map = () => {
+  return (
+    <Fragment>
+      {" "}
+      <Seo />
+      <script type="text/javascript"></script>
+      <div style={{
+
+        display: "flex",
+        justifyContent: "center",
+        alignContent: "center",
+        padding: 50
+      }}>
+
+        <form action="//www.google.com/maps" id="">
+          <input aria-label="Your Location" id="" name="saddr" placeholder="FROM" type={"text"} ></input>
+          <input aria-label="Our Location" id="" name="daddr" placeholder="TO" type={"hidden"} value={'Memorial+Student+Center,+275+Joe+Routt+Blvd,+College+Station,+TX+77840'}></input>
+          <input type={"submit"} value="Get Distance" formTarget="_blank"></input>
+
+        </form>
+
+        <iframe
+          width="800"
+          height="550"
+          style={{ border: "0" }}
+          loading="lazy"
+          allowFullScreen
+          referrerpolicy="no-referrer-when-downgrade"
+          src={`https://www.google.com/maps/embed/v1/place?key=${MAP_KEY}&q=Memorial+Student+Center,+275+Joe+Routt+Blvd,+College+Station,+TX+77840`}
+        ></iframe>
+      </div>
+    </Fragment>
+  );
+};
+
+export default Map;
